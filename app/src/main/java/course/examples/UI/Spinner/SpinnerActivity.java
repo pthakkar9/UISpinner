0f46692c1f257d3c2000bc3993cffe1bd5ad8cb6
Parva Thakkar <parva.thakkar@gmail.com>
2014-10-25 11:11:22 -0400

    first and last commit

---
+++ app/src/main/java/course/examples/UI/Spinner/SpinnerActivity.java
@@ -0,0 +1,59 @@
+package course.examples.UI.Spinner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SpinnerActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        // Get a reference to the Spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Create an Adapter that holds a list of colors
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.colors, R.layout.dropdown_item);
+
+        // Set the Adapter for the spinner
+        spinner.setAdapter(adapter);
+
+        // Set an setOnItemSelectedListener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                // Display a Toast message indicating the currently selected
+                // item
+                Toast.makeText(
+                        parent.getContext(),
+                        "The color is "
+                                + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, R.layout.dropdown_item,
+                getResources().getStringArray(R.array.colors)));
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+    }
+}
